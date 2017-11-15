b620b20a625e22c18358cd3e6d2c7151b78ff85d
mingmingcome <haomingluhello@gmail.com>
2017-11-15 08:59:10 +0800

    strings

---
+++ strings/JGrep.java
@@ -0,0 +1,25 @@
+// 2017-11-14 20:10:21
+// A very simple version of the "grep" program.
+// {Args: JGrep.java "\\b[Ssct]\\w+"}
+import java.util.regex.*;
+
+public class JGrep {
+    public static  void main(String[] args) throws Exception {
+        if(args.length < 2) {
+            System.out.println("Usage: java JGrep file regex");
+            System.exit(0);
+        }
+        Pattern p = Pattern.compile("\\b[Ssct]\\w+");
+        System.out.println(args[1]);
+        // Iterate through the lines of the input file:
+        int index = 0;
+        Matcher m = p.matcher("");
+        for(String line : new TextFile(args[0])) {
+            m.reset(line);
+            while(m.find()) {
+                System.out.println(index++ + ": " + 
+                    m.group() + ": " + m.start());
+            }
+        }
+    }
+}
